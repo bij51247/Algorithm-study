4846e64cc69cad90bd0dc4e34be673b333303824
Tsutomu Kimura <kmrttm247810@gmail.com>
2021-03-11 10:31:05 +0900

    修正

--- sort.py
+++ sort.py
@@ -1,17 +1,26 @@
 class InertionSort:
-    #コンストラクタ
+    # コンストラクタ
     def __init__(self, num):
         self.num = num
-    #関数
+    # 関数
     def printArray(self):
-        print(self.num)
+        print('ソート後',self.num)
+
 
 numlist = [10, 43, 3, 24, 5]
 
-print('ソート前',numlist)
+print('ソート前', numlist)
 
+for i in range(1,len(numlist)):
+    temp = numlist[i]
+    cmp = i - 1
+    while cmp >=0:
+        if(numlist[cmp]>temp):
+            numlist[cmp+1] =numlist[cmp]
+        cmp-=1
+    numlist[cmp+1] = temp
 
-#インスタンス作成
+# インスタンス作成
 insert = InertionSort(numlist)
-#呼び出し
+# 呼び出し
 insert.printArray()
