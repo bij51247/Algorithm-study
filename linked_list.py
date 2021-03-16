1537671a420de227093be248d0a4d01910563cae
Tsutomu Kimura <kmrttm247810@gmail.com>
2021-03-16 09:08:46 +0900

    連結リンクno

---
+++ linked_list.py
@@ -0,0 +1,31 @@
+class Cell:
+  def __init__(self,value):
+    self.value = value
+    self.next = None
+
+class LinkedList:
+  def __init__(self):
+    self.head = Cell(None)
+  
+  def insert(self,value):
+    front = self.head
+    rear  = front.next
+    
+    while rear and value > rear.value:
+      front = rear
+      rear = rear.next
+    cell = Cell(value)
+    cell.next = rear
+    front.next = cell
+    
+  def show(self):
+    tmp = self.head.next
+    while tmp:
+      print(tmp.value)
+      tmp = tmp.next
+
+link = LinkedList()
+link.insert(5)
+link.insert(3)
+
+link.show()
--- sort.py
+++ sort.py
@@ -13,11 +13,15 @@ print('ソート前', numlist)
 
 for i in range(1,len(numlist)):
     temp = numlist[i]
+    print('temp',temp)
     cmp = i - 1
+    print('cmp',cmp)
     while cmp >=0:
         if(numlist[cmp]>temp):
             numlist[cmp+1] =numlist[cmp]
+        print('cmp-1にした結果')
         cmp-=1
+        print(cmp)
     numlist[cmp+1] = temp
 
 # インスタンス作成
